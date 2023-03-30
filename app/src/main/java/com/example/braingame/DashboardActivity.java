5719c1bac96ec6b5e7f5440cefe0a388ce448b6c
Jim <jimmusfique@gmail.com>
2023-03-31 02:38:58 +0600

    DashboardActivity To Mainactivity

--- app/src/main/java/com/example/braingame/DashboardActivity.java
+++ app/src/main/java/com/example/braingame/DashboardActivity.java
@@ -2,13 +2,46 @@ package com.example.braingame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class DashboardActivity extends AppCompatActivity {
+CountDownTimer countDownTimer;
+int timerValue = 20;
+ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        progressBar = findViewById(R.id.quiz_timer);
+        countDownTimer = new CountDownTimer(20000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerValue = timerValue -1;
+                progressBar.setProgress(timerValue);
+            }
+
+            @Override
+            public void onFinish() {
+                Dialog dialog = new Dialog(DashboardActivity.this,R.style.DiaLoge);
+                dialog.setContentView(R.layout.time_out_dialog);
+                dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+                dialog.show();
+
+                dialog.findViewById(R.id.btn_ttyaAain).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(DashboardActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }.start();
     }
 }
